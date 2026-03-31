98c4e26d582e3597c3dd80750fc9b743b3f1f9e4
Ucenik <Ucenik@DESKTOP-VTJ1B2M>
2026-03-31 11:11:27 +0200

    cas 31.03

--- Konekcija.cs
+++ Konekcija.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace projekat_2026_Andjela_Stoisavljevic
 {
-    internal class Konekcija
+    public class Konekcija
     {
+        static public SqlConnection connect()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["skola"];
+            return null;
+        }
     }
 }
--- Login.cs
+++ Login.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace projekat_2026_Andjela_Stoisavljevic
@@ -25,7 +27,25 @@ namespace projekat_2026_Andjela_Stoisavljevic
 
         private void btnOK_Click(object sender, EventArgs e)
         {
-
+            SqlConnection veza = Konekcija.povezi();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM korisnik WHERE email='" + txtEmail.Text + "'", veza);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable tabela = new DataTable();
+            adapter.Fill(tabela);
+            int broj = tabela.Rows.Count;
+            if (broj == 0)
+            {
+                MessageBox.Show("Nepostojeci email");
+            }
+            else
+            {
+                if (txtPass.Text == tabela.Rows[0]["pass"].ToString())
+                {
+                    Glavna nova = new Glavna();
+                    this.Hide();
+                    nova.Show();
+                }
+                else MessageBox.Show("Nije dobra lozinka!");
+            }
         }
-    }
 }
